01b90509333b51bcc5c24398ba1a3f50aeb4ae7d
Colby Cardell <Colby88cardell@gmail.com>
2021-01-26 16:37:19 -0500

    :poop: toggled hidden fields

--- scripts.js
+++ scripts.js
@@ -1,9 +1,17 @@
-const usernameInput = document.querySelector("#uname");
+const toggleBtn = document.querySelector("#toggle-Btn");
+const submitBtn = document.querySelector('button[type="submit"]');
+const toggledInputs = document.querySelectorAll("input:not(#uname, #pass)");
 
-usernameInput.addEventListener(
-  "input",
+for (let i = 0; i < toggledInputs.length; i += 1) {
+  toggledInputs[i].classList.add("is-hidden");
+}
 
-  () => {
-    console.log("hello! I was changed");
+toggleBtn.addEventListener("click", function () {
+  if (this.innerText === "Already have an account") {
+    this.innerText = "Need to create an account?";
+    submitBtn.innerText = "Login!";
+  } else {
+    this.innerText = "Already have an account?";
+    submitBtn.innerText = "Register!";
   }
-);
+});
