2487907970a984704dc96091634e13be118bbb43
Colby Cardell <Colby88cardell@gmail.com>
2021-01-27 05:06:29 -0500

    :bug: fixed iss with both hidden an vissible classes/changed query selector to render on chrome

--- scripts.js
+++ scripts.js
@@ -1,15 +1,26 @@
-const toggleBtn = document.querySelector("#toggle-Btn");
 const submitBtn = document.querySelector('button[type="submit"]');
-const toggledInputs = document.querySelectorAll("input:not(#uname, #pass)");
+const toggleBtn = document.querySelector("#toggle-btn");
+
+const toggledInputs = Array.from(document.querySelectorAll("input")).slice(
+  0,
+  4
+);
 
 toggleBtn.addEventListener("click", function () {
-  if (this.innerText === "Already have an account") {
+  if (this.innerText === "Already have an account?") {
     for (let i = 0; i < toggledInputs.length; i += 1) {
+      toggledInputs[i].classList.remove("is-visible");
       toggledInputs[i].classList.add("is-hidden");
     }
     this.innerText = "Need to create an account?";
     submitBtn.innerText = "Login!";
   } else {
+    const hiddenInputs = document.querySelectorAll("input.is-hidden");
+    for (let i = 0; i < hiddenInputs.length; i += 1) {
+      toggledInputs[i].classList.remove("is-hidden");
+      hiddenInputs[i].classList.add("is-visible");
+    }
+
     this.innerText = "Already have an account?";
     submitBtn.innerText = "Register!";
   }
